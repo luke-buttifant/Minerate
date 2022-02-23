136f52b2674a022f640195094d8817f0b9bfb336
Luke <lukerhys.lb@gmail.com>
2022-02-23 01:44:08 +0000

    false wallet ID error handling

---
+++ dist/popup.e9c31bd7.js
@@ -0,0 +1,27 @@
+baseUrl = 'https://eth.2miners.com/api/accounts/';
+window.onload = function() {
+    const table = document.getElementById("table");
+    const walletIdInput = document.getElementById("walletID");
+    const IdLabel = document.getElementById("IdLabel");
+    const requestBtn = document.getElementById("requestBtn");
+    const alert = document.getElementById("alert");
+    var toggled = 0;
+    requestBtn.onclick = async ()=>{
+        try {
+            id = walletIdInput.value;
+            const request = baseUrl + id;
+            const response = await fetch(request);
+            const data = await response.json();
+            document.getElementById("24hReward").innerHTML = data['24hreward'];
+            document.getElementById("balance").innerHTML = data['stats']['balance'];
+            document.getElementById("workersOnline").innerHTML = data['workersOnline'];
+            table.classList.toggle('hidden');
+            walletIdInput.classList.toggle('hidden');
+            IdLabel.classList.toggle('hidden');
+        } catch  {
+            alert.classList.toggle("hidden");
+        }
+    };
+};
+
+//# sourceMappingURL=popup.e9c31bd7.js.map
--- src/popup.js
+++ src/popup.js
@@ -6,20 +6,32 @@ window.onload = function(){
     const walletIdInput = document.getElementById("walletID");
     const IdLabel = document.getElementById("IdLabel");
     const requestBtn = document.getElementById("requestBtn");
+    const alert = document.getElementById("alert");
+    var toggled = 0;
 
     requestBtn.onclick = async () => {
-        id = walletIdInput.value;
-        const request = baseUrl + id
-        const response = await fetch(request);
-        const data = await response.json();
-        document.getElementById("24hReward").innerHTML = data['24hreward'];
-        document.getElementById("balance").innerHTML = data['stats']['balance'];
-        document.getElementById("workersOnline").innerHTML =  data['workersOnline'];
+        try{
+            id = walletIdInput.value;
+            const request = baseUrl + id
+            const response = await fetch(request);
+            const data = await response.json();
+            document.getElementById("24hReward").innerHTML = data['24hreward'];
+            document.getElementById("balance").innerHTML = data['stats']['balance'];
+            document.getElementById("workersOnline").innerHTML =  data['workersOnline'];
+    
+    
+            table.classList.toggle('hidden');
+            walletIdInput.classList.toggle('hidden');
+            IdLabel.classList.toggle('hidden');
+            alert.classList.toggle("hidden");
+        }
+        catch{
+            if(toggled < 1){
+                alert.classList.toggle("hidden");
+            }
+            toggled += 1;
+        }
 
-
-        table.classList.toggle('hidden');
-        walletIdInput.classList.toggle('hidden');
-        IdLabel.classList.toggle('hidden');
     }
 }
 
