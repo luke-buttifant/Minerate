2848bf77e8f382ce6e96d15ee64d1c361549dab8
luke-buttifant <lukerhys.lb@gmail.com>
2022-02-25 17:54:24 +0000

    added wallet link to values

--- dist/popup.39c042f9.js
+++ dist/popup.39c042f9.js
@@ -1,64 +1,48 @@
-
-
-baseUrl = 'api/accounts/'
-
-window.onload = function(){
+baseUrl = 'api/accounts/';
+window.onload = function() {
     const select = document.getElementById('serverOptions');
-
     const table = document.getElementById("table");
     const walletIdInput = document.getElementById("walletID");
     const IdLabel = document.getElementById("IdLabel");
     const requestBtn = document.getElementById("requestBtn");
     const alert = document.getElementById("alert");
     const optionsLbl = document.getElementById("optionsLabel");
-
-    requestBtn.onclick = async () => {
+    requestBtn.onclick = async ()=>{
         var frequency = 'MH/s';
-        try{
+        try {
             var server = select.options[select.selectedIndex].value;
             id = walletIdInput.value;
             var request = server + baseUrl + id;
             const response = await fetch(request);
             const data = await response.json();
-            var r_24Reward = (data['24hreward'] / 1000000000).toFixed(6)
-            var r_balance = (data['stats']['balance'] / 1000000000).toFixed(6)
-            var r_hashrate = (data['currentHashrate'] / 1000000).toFixed(2)
-            if(r_hashrate > 1000){
-                r_hashrate = (r_hashrate/ 1000).toFixed(2)
+            var r_24Reward = (data['24hreward'] / 1000000000).toFixed(6);
+            var r_balance = (data['stats']['balance'] / 1000000000).toFixed(6);
+            var r_hashrate = (data['currentHashrate'] / 1000000).toFixed(2);
+            if (r_hashrate > 1000) {
+                r_hashrate = (r_hashrate / 1000).toFixed(2);
                 frequency = 'GH/s';
             }
-            
-            var t_24RewardDiv = document.getElementById("24hReward")
+            var t_24RewardDiv = document.getElementById("24hReward");
             t_24RewardDiv.innerHTML = r_24Reward;
             t_24RewardDiv.href = server + "account/" + id;
-
-            var t_balanceDiv = document.getElementById("balance")
-            t_balanceDiv.innerHTML = r_balance;
-            t_balanceDiv.href = t_24RewardDiv.href = server + "account/" + id;
-
-            var t_workersOnlineDiv = document.getElementById("workersOnline");
-            t_workersOnlineDiv.innerHTML =  data['workersOnline'];
-            t_workersOnlineDiv.href = server + "account/" + id;
-
-            var t_hashrateDiv = document.getElementById("hashrate")
-            t_hashrateDiv.innerHTML =  r_hashrate + frequency;
-            t_hashrateDiv.href = server + "account/" + id;
-    
+            var t_balanceDiv = document.getElementById("balance");
+            balanceDiv.innerHTML = r_balance;
+            balanceDiv.href = t_24RewardDiv.href = server + "account/" + id;
+            var workersOnlineDiv;
+            document.getElementById("workersOnline").innerHTML = data['workersOnline'];
+            document.getElementById("hashrate").innerHTML = r_hashrate + frequency;
             table.classList.toggle('hidden');
             walletIdInput.classList.toggle('hidden');
             IdLabel.classList.toggle('hidden');
             alert.classList.add("hidden");
             select.classList.toggle("hidden");
             optionsLbl.classList.toggle("hidden");
-        }
-        catch{
+        } catch  {
             alert.classList.remove("hidden");
             select.classList.remove("hidden");
             optionsLbl.classList.remove("hidden");
         }
+    };
+};
 
-    }
-
-}
-
-
+//# sourceMappingURL=popup.39c042f9.js.map
