102f78d7da6ee392d6ededc96d42449b918e3bd6
luke-buttifant <lukerhys.lb@gmail.com>
2022-02-26 02:56:12 +0000

    bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.js"],
-  content: [],
+  content: ["./src/**/*.js"],
   theme: {
     extend: {
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
       colors: {
         "primary": "#ff2e00",
         "secondary": '#0d1117',
